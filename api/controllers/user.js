50c510d6d258eb102a4a7b19bd4b6dfbe22f2fd0
Edward Kurniady <t-edward.kurniady@ovo.id>
2019-11-13 13:26:23 +0700

    add password encryption

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,12 +3,14 @@ const Bounce = require('bounce');
 const Model = require(path.resolve('.', 'models'));
 const constants = require(path.resolve('.', 'const'));
 const {
+  crypt,
   normalize,
 } = require(path.resolve('.', 'utils'));
 
 module.exports.login = async (req, h) => {
   const payload = { ...req.payload };
   payload.phoneNumber = normalize.phoneNumber(payload.phoneNumber);
+  payload.password = crypt.encrypt(payload.password);
   
   try {
     const user = await Model.User.findOne({
@@ -24,7 +26,10 @@ module.exports.login = async (req, h) => {
     if (user && user.password != payload.password) errResp.message = 'Wrong password!';
     if (errResp.message) return errResp;
 
-    return constants['200'];
+    return {
+      ...constants['200'],
+      cookie: '',
+    };
 
   } catch(e) {
     console.error(e);
@@ -36,6 +41,7 @@ module.exports.login = async (req, h) => {
 module.exports.register = async (req, h) => {
   const payload = { ...req.payload };
   payload.phoneNumber = normalize.phoneNumber(payload.phoneNumber);
+  payload.password = crypt.encrypt(payload.password);
 
   try {
     const user = await Model.User.findOne({
