b57abfe58ba0471627d14bf9331434f8b7feb9f6
Edward Kurniady <t-edward.kurniady@ovo.id>
2019-12-02 16:33:42 +0700

    fix register

--- api/services/user.js
+++ api/services/user.js
@@ -2,6 +2,7 @@ const path = require('path');
 const root = path.resolve('.');
 const Model = require(`${root}/models`);
 const { photo } = require(`${root}/utils`);
+const { storageURL } = require(`${root}/const`);
 
 module.exports.login = async (creds) => {
   return await Model.User.findOne({
@@ -14,13 +15,17 @@ module.exports.login = async (creds) => {
 
 module.exports.register = async (data) => {
   data.isWalker = false;
-  data.photo = await photo.upload(data.photo, data.id, 'profile');
-
+  
   const newData = {};
   Object.keys(Model.User.rawAttributes).forEach(attb => {
-    newData[attb] = data[attb];
+    newData[attb] = attb === 'photo' ? '' : data[attb];
   });
-  await Model.User.create(newData);
+  const user = await Model.User.create(newData);
+  
+  await photo.upload(data.photo, user.id, 'profile');
+  user.update({
+    photo: `${storageURL}/${process.env.BUCKET_NAME}/${user.id}/profile`,
+  });
 };
 
 module.exports.update = async (data) => {
