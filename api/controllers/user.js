bb90ba278f1d83f9138cf6a76feb92d90f155185
Edward Kurniady <t-edward.kurniady@ovo.id>
2019-12-03 13:20:28 +0700

    improve user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const root = path.resolve('.');
-const Bounce = require('bounce');
 const jwt = require('jsonwebtoken');
 const constants = require(`${root}/const`);
 const {
@@ -14,110 +13,86 @@ const {
 } = require(`${root}/utils`);
 
 module.exports.login = async (req, h) => {
-  try {
-    const payload = processCred(req.payload);
-    const errResp = {
-      message: null,
-      ...constants['401'],
-    };
+  const payload = processCred(req.payload);
+  const errResp = {
+    message: null,
+    ...constants['401'],
+  };
 
-    const usr = await user.login(payload);
-    if (!usr) errResp.message = 'Phone number not registered!';
-    if (usr && usr.password != payload.password) errResp.message = 'Wrong password!';
-    if (errResp.message) return errResp;
+  const usr = await user.login(payload);
+  if (!usr) errResp.message = 'Phone number not registered!';
+  if (usr && usr.password != payload.password) errResp.message = 'Wrong password!';
+  if (errResp.message) return errResp;
 
-    return {
-      ...constants['200'],
-      commonResponse: {
-        ...usr,
-        dogs: await dog.getList(usr.id),
+  return {
+    ...constants['200'],
+    commonResponse: {
+      ...usr,
+      dogs: await dog.getList(usr.id),
+    },
+    session: jwt.sign({
+      user: {
+        id: usr.id,
       },
-      session: jwt.sign({
-        user: {
-          id: usr.id,
-        },
-      }, process.env.JWT_KEY, { expiresIn: process.env.JWT_EXPIRATION }),
-    };
-  } catch(e) {
-    console.error(e);
-    Bounce.rethrow(e, 'boom');
-    Bounce.rethrow(e, 'system');
-  }
+    }, process.env.JWT_KEY, { expiresIn: process.env.JWT_EXPIRATION }),
+  };
 };
 
 module.exports.register = async (req, h) => {
-  try {
-    const payload = processCred(req.payload);
-    const {
-      key,
-      duplicate,
-    } = await dupCheck('User', payload);
+  const payload = processCred(req.payload);
+  const {
+    key,
+    duplicate,
+  } = await dupCheck('User', payload);
 
-    if (duplicate) return {
-      ...constants['409'],
-      message: `duplicate ${key}!`,
-    };
+  if (duplicate) return {
+    ...constants['409'],
+    message: `duplicate ${key}!`,
+  };
 
-    await user.register(payload);
-    
-    return constants['200'];
-  } catch(e) {
-    console.error(e);
-    Bounce.rethrow(e, 'boom');
-    Bounce.rethrow(e, 'system');
-  }
+  await user.register(payload);
+  
+  return constants['200'];
 };
 
 module.exports.update = async (req, h) => {
-  try {
-    const session = auth.verify(req.headers.session);
-    if (session.error) return {
-      ...constants['401'],
-      message: session.error,
-    };
+  const session = auth.verify(req.headers.session);
+  if (session.error) return {
+    ...constants['401'],
+    message: session.error,
+  };
 
-    const payload = processCred(req.payload);
-    payload.id = session.user.id;
-    const {
-      key,
-      duplicate,
-    } = await dupCheck('User', payload);
-    const isUserData = duplicate ? duplicate.id === payload.id : false;
-    
-    if (duplicate && !isUserData) return {
-      ...constants['409'],
-      message: `duplicate ${key}!`,
-    };
-    await user.update(payload);
-    
-    return {
-      ...constants['200'],
-      session: auth.refresh(session),
-    };
-  } catch(e) {
-    console.error(e);
-    Bounce.rethrow(e, 'boom');
-    Bounce.rethrow(e, 'system');
-  }
+  const payload = processCred(req.payload);
+  payload.id = session.user.id;
+  const {
+    key,
+    duplicate,
+  } = await dupCheck('User', payload);
+  const isUserData = duplicate ? duplicate.id === payload.id : false;
+
+  if (duplicate && !isUserData) return {
+    ...constants['409'],
+    message: `duplicate ${key}!`,
+  };
+  await user.update(payload);
+  
+  return {
+    ...constants['200'],
+    session: auth.refresh(session),
+  };
 };
 
 module.exports.get = async (req, h) => {
-  try {
-    const session = auth.verify(req.headers.session);
-    if (session.error) return {
-      ...constants['401'],
-      message: session.error,
-    };
-  
-    const usr = await user.get(req.params.user || session.user.id);
-    return {
-      profile: usr,
-      dogs: await dog.getList(usr.id),
-      session: auth.refresh(session),
-    };
-  } catch(e) {
-    console.error(e);
-    Bounce.rethrow(e, 'boom');
-    Bounce.rethrow(e, 'system');
-  }
+  const session = auth.verify(req.headers.session);
+  if (session.error) return {
+    ...constants['401'],
+    message: session.error,
+  };
+
+  const usr = await user.get(req.params.user || session.user.id);
+  return {
+    profile: usr,
+    dogs: await dog.getList(usr.id),
+    session: auth.refresh(session),
+  };
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const root = path.resolve('.');
+const Bounce = require('bounce');
+const constants = require(`${root}/const`);
+const { auth } = require(`${root}/utils`);
 
 const basename = path.basename(module.filename);
 
 const routes = [];
+const noAuth = {
+  user: [
+    'login',
+    'register',
+  ],
+};
 
 fs.readdirSync(__dirname)
   .filter((file) => 
@@ -12,7 +22,34 @@ fs.readdirSync(__dirname)
     && file.slice(-3) === '.js')
   .forEach((file) => {
     const name = file.slice(0, -3);
-    routes.push(...require(`./${name}`));
+    routes.push(...require(`./${name}`).map(route => {
+      const fn = route.config.handler;
+      route.config.handler = async (req, h) => {
+        try {
+          let needAuth = true;
+          noAuth[name].forEach(na => {
+            if (route.path.indexOf(na) > -1) needAuth = false;
+          });
+
+          const session = auth.verify(req.headers.session);
+          if (needAuth && session.error) return {
+            ...constants['401'],
+            message: session.error,
+          };
+
+          const result = await fn(req, h);
+          return needAuth ? {
+            ...result,
+            session: auth.refresh(session),
+          } : result;
+        } catch(e) {
+          console.error(e);
+          Bounce.rethrow(e, 'boom');
+          Bounce.rethrow(e, 'system');
+        }
+      }
+      return route;
+    }));
   });
 
 module.exports = routes;
