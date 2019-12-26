bfc27ea02c2d9ec4475aab0b08350493dc89d1ca
Edward Kurniady <t-edward.kurniady@ovo.id>
2019-12-26 14:07:42 +0700

    abcdasdef

--- api/routes/firebase.js
+++ api/routes/firebase.js
@@ -1,19 +1,19 @@
-// const base = 'firebase';
-// const controller = require('../controllers')[base];
+const base = 'firebase';
+const controller = require('../controllers')[base];
 
-// module.exports = [
-//   {
-//     method: 'POST',
-//     path: `/${base}/notification`,
-//     config: {
-//       handler: controller.login,
-//       validate: {
-//         payload: {
-//           password: Joi.string().required(),
-//           phoneNumber: Joi.string().required(),
-//           token: Joi.string().allow(''),
-//         },
-//       },
-//     },
-//   },
-// ];
+module.exports = [
+  // {
+  //   method: 'POST',
+  //   path: `/${base}/notification`,
+  //   config: {
+  //     handler: controller.login,
+  //     validate: {
+  //       payload: {
+  //         password: Joi.string().required(),
+  //         phoneNumber: Joi.string().required(),
+  //         token: Joi.string().allow(''),
+  //       },
+  //     },
+  //   },
+  // },
+];
