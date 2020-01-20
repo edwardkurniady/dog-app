004f8bb57d31f6e60295967bc0759a9eb6d9d58f
Edward Kurniady <t-edward.kurniady@ovo.id>
2020-01-20 15:19:26 +0700

    bb

--- api/services/photo.js
+++ api/services/photo.js
@@ -55,12 +55,12 @@ module.exports.update = async (type, oldId, newId) => {
   const oldFile = bucket.file(oldName);
   await oldFile.copy(newName);
   await oldFile.delete();
-  
+  try{
   return (await bucket.file(newName).getSignedUrl({
     action: 'read',
     version: 'v2',
     expires: '03-17-3000'
-  }))[0];
+  }))[0];}catch(E) {console.log(E)}
 };
 
 module.exports.delete = async (id, type) => {
